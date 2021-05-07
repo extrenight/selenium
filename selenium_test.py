99a1f7316ead74ad93735e1334cb815ec5a9f1f7
extrenight <82199284+extrenight@users.noreply.github.com>
2021-05-07 17:28:46 +0800

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,20 +1,42 @@
 #coding = utf-8
+import pytest
 from selenium import webdriver
-from time import sleep
+from time import sleep, ctime
+import os
 
 options = webdriver.ChromeOptions()
-#这里是chrome浏览器可执行文件的地址(一般在C:\Program Files\Google,下)
-options.binary_location = "D:/Google/Chrome/Application/chrome.exe"
-#这里是浏览器驱动地址
-chrome_driver_binary = "D:/Google/Chrome/Application/chromedriver.exe"
+options.binary_location = "C:/Program Files/Google/Chrome/Application/chrome.exe"
+chrome_driver_binary = "D:/Google/chromedriver.exe"
 driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
-#要测试的网页，打开网页
-driver.get("https://www.baidu.com/")
-sleep(3)
-#找到id为“kw”的控件，输入测试文本 Test search
-driver.find_element_by_id("kw").send_keys("Test search")
-#找到id为”su“的控件，模拟鼠标点击
-driver.find_element_by_id("su").click()
-sleep(3)
-#退出
-driver.quit()
+
+
+class TestCase():
+    def setup_method(self):
+        driver.get("https://www.baidu.com/")
+        sleep(3)
+
+    def teardown_method(self):
+        sleep(3)
+
+    def teardown_class(self):
+        sleep(3)
+        driver.quit()
+
+    def test_one(self):
+        driver.find_element_by_id("kw").send_keys("Test search")
+        driver.find_element_by_id("su").click()
+
+    def test_two(self):
+        driver.find_element_by_id("kw").send_keys("***")
+        driver.find_element_by_id("su").click()
+
+    def test_three(self):
+        driver.find_element_by_id("kw").send_keys("@")
+        driver.find_element_by_id("su").click()
+
+    def test_four(self):
+        driver.find_element_by_id("kw").send_keys(" ")
+        driver.find_element_by_id("su").click()
+
+    def test_five(self):
+        driver.find_element_by_link_text("新闻").click()
